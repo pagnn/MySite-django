5a6101703036aa383b9421431a20a0c0e92668c1
Sylvia <sylviawei19950920@gmail.com>
2017-12-26 14:11:47 +0800

    templates

--- src/account/urls.py
+++ src/account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'login/$',user_login,name='user_login'),
     url(r'logout/$',auth_views.logout,{'template_name':'account/logout.html'},name='user_logout'),
     url(r'register/$',register,name='register'),
+    url(r'password-change/$',auth_views.password_change,{'template_name':'account/password_change.html'},name='password_change'),
+    url(r'password-change-done/$',auth_views.password_change_done,{'template_name':'account/password_change_done.html'},name='password_change_done')
 ]
